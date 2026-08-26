6353a389367075eabb09afbba4470f824beb2b47
Yash Raj <rajyashkarn@gmail.com>
2026-08-26 12:34:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (24.85%) - LeetSync

---
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.c
@@ -0,0 +1,11 @@
+bool checkDivisibility(int n) {
+    int sum = 0,product = 1;
+    int temp = n;
+    while(temp!=0){
+        int rem = temp%10;
+        sum += rem;
+        product*= rem;
+        temp/= 10;
+    }
+    return n%(sum+product)==0;
+}
