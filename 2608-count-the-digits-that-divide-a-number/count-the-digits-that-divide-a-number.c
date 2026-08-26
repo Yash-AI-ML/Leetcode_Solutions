e391bb7c0b47b50233c15a80f638b0f7b5a64033
Yash Raj <rajyashkarn@gmail.com>
2026-08-26 13:08:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (99.72%) - LeetSync

---
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.c
@@ -0,0 +1,12 @@
+int countDigits(int num) {
+    int count = 0;
+    int temp = num;
+    while (temp!=0){
+        int rem = temp %10;
+        if(num%rem == 0 && rem!=0){
+            count++;
+        }
+        temp/=10;
+    }
+    return count;
+}
