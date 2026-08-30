d04c121e958b81f2823be0a4451dfd4be871cc96
Yash Raj <rajyashkarn@gmail.com>
2026-08-30 20:37:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (21.19%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.c
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.c
@@ -1,5 +1,8 @@
 bool isSameAfterReversals(int num) {
-    if((num%10!=0) || (num == 0)){
+    if(num==0){
+        return true;
+    }
+    if(num%10!=0){
         return true;
     }
     return false;
